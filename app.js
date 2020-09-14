5e68e99861aefc08191fc15c7844c9da56be8de8
hiroshi sato <40625222+heavygears@users.noreply.github.com>
2020-09-14 20:36:20 +0900

    イベント発生で無名関数を呼び出し

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const fs = require ('fs');
 const readline = require ('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
 const rl = readline.createInterface({ input: rs, output: {} });
+rl.on('line', lineString => {
+  console.log(lineString);
+});
