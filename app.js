ab456562b4c45408bc7f41b45cb6e81dd8e34fb4
hiroshi sato <40625222+heavygears@users.noreply.github.com>
2020-09-14 20:34:48 +0900

    ファイル読み込み、オブジェクト作成

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 'use strict';
 const fs = require ('fs');
 const readline = require ('readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ input: rs, output: {} });
